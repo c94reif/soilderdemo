bb4bc9ec2bc459db22a135b0383ee028b530ce77
Christopher Reif <c94reif@outlook.com>
2024-09-19 13:13:40 -0500

    Start Here

---
+++ src/main/java/com/example/SoilderDemo/SoilderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoilderDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoilderDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SoilderDemo/SoilderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoilderDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoilderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
